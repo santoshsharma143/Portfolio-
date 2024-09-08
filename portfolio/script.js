a0bf3db1fcecfe31f653a78fc311e5f3239bfd36
Santosh Kumar Sharma <108514898+santoshsharma143@users.noreply.github.com>
2024-09-08 12:08:28 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+   strings:["Frontend Developer", " Youtuber","Python Programmer" ],
+   typespeed:10000,
+   backspeed:10000,
+   backDelay:1000,
+   loop: true
+}) 
